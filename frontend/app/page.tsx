35ade4263771bdf2edcdbe78d6fb1d4acb334b69
braverokmc79 <braverokmc79@gmail.com>
2025-07-10 10:42:38 +0900

    setting tailwind

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind 4.1 + Next.js 15 시작!</h1>
+    
+      <div className="bg-airbnb airbnb  p-6">
+          Airbnb 스타일 배경입니다
+    </div>
+
+    
+    </main> 
+  );
+}
