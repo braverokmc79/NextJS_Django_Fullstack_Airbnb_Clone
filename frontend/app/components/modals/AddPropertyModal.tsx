00a2e16d4be01142b35aa0fcb38e0d36b3fe99a2
braverokmc79 <braverokmc79@gmail.com>
2025-07-15 21:32:27 +0900

    5_4.숙서 상세 정보 입력

--- frontend/app/components/modals/AddPropertyModal.tsx
+++ frontend/app/components/modals/AddPropertyModal.tsx
@@ -10,8 +10,16 @@ import Categories from '../addproperty/Categories';
 const AddPropertyModal: React.FC = () => {
   const [currentStep, setCurrentStep] = useState(1);
   const [dataCategory, setDataCategory] = useState('');
-  const [dataTitle, setDataTitle] = useState('');
+  const [dataTitle, setDataTitle] = useState('');  
   const [dataDescription, setDataDescription] = useState('');
+
+  const [dataPrice, setDataPrice] = useState('');
+  const [dataBedrooms, setDataBedrooms] = useState('');
+  const [dataBathrooms, setDataBathrooms] = useState('');
+  const [dataGuests, setDataGuests] = useState('');
+
+
+
 
   const addPropertyModal = useAddPropertyModal();
 
@@ -46,56 +54,120 @@ const AddPropertyModal: React.FC = () => {
             e.preventDefault();
             setCurrentStep(3)
         }}>
-          <h2 className="mb-6 text-2xl">장소를 선택하세요!</h2>    
-        
-         <div className='pt-3 pb-6 space-y-4'>
-            <div className='flex flex-col space-y-2'>
-                <label>제목</label>
-                <input 
-                  type="text"
-                  value={dataTitle}
-                  required
-                  onChange={(e) => setDataTitle(e.target.value)}
-                  className='w-full p-2 bg-gray-200 rounded-xl'
-                />
-            </div>
+            <h2 className="mb-6 text-2xl">장소를 선택하세요!</h2>    
+          
+          <div className='pt-3 pb-6 space-y-4'>
+              <div className='flex flex-col space-y-2'>
+                  <label>제목</label>
+                  <input 
+                    type="text"
+                    value={dataTitle}
+                    required
+                    onChange={(e) => setDataTitle(e.target.value)}
+                    className='w-full p-2 bg-gray-200 rounded-xl'
+                  />
+              </div>
 
-            <div className='flex flex-col space-y-2'>
-                <label>내용</label>
-                <textarea                  
-                  value={dataDescription}
-                  required
-                  onChange={(e) => setDataDescription(e.target.value)}
-                  className='w-full h-[200px]  p-4  border border-gray-600  bg-gray-200 rounded-xl'
-                />
-            </div>
-         </div>
+              <div className='flex flex-col space-y-2'>
+                  <label>내용</label>
+                  <textarea                  
+                    value={dataDescription}
+                    required
+                    onChange={(e) => setDataDescription(e.target.value)}
+                    className='w-full h-[200px]  p-4  border border-gray-600  bg-gray-200 rounded-xl'
+                  />
+              </div>
+          </div>
 
-          <CustomButton   
-              label="이전"
-              className="mb-2 !bg-black hover:!bg-gray-800"
-              onClick={() => setCurrentStep(1)}
-            /> 
+            <CustomButton   
+                label="이전"
+                className="mb-2 !bg-black hover:!bg-gray-800"
+                onClick={() => setCurrentStep(1)}
+              /> 
 
-           <CustomButton
-              label="다음"
-              type='submit'              
-            />  
-          </form>  
-        </>
-      ) :(
-        <>
-        <p> 
-           adsafsd  </p>
-          <CustomButton
-            label="다음"
-            onClick={() => setCurrentStep(4)}
-          />            
+            <CustomButton
+                label="다음"
+                type='submit'              
+              />  
+            </form>  
+          </>
+        ) : currentStep === 3 ?(
+          <>
+
+<form onSubmit={(e) => {
+  e.preventDefault();
+  setCurrentStep(4);
+}}>
+  <h2 className="mb-6 text-2xl">숙소 상세 정보</h2>
+
+  <div className="pt-3 pb-6 space-y-4">
+    <div className="flex flex-col space-y-2">
+      <label>1박 요금 (₩)</label>
+      <input
+        type="number"
+        required
+        value={dataPrice}
+        onChange={(e) => setDataPrice(e.target.value)}
+        className="w-full p-4 border border-gray-600 rounded-xl"
+      />
+    </div>
+
+    <div className="flex flex-col space-y-2">
+      <label>침실 수</label>
+      <input
+        type="number"
+        required
+        max={5}
+        value={dataBedrooms}
+        onChange={(e) => setDataBedrooms(e.target.value)}
+        className="w-full p-4 border border-gray-600 rounded-xl"
+      />
+    </div>
+
+    <div className="flex flex-col space-y-2">
+      <label>욕실 수</label>
+      <input
+        type="number"
+        required
+        value={dataBathrooms}
+        max={3}
+        onChange={(e) => setDataBathrooms(e.target.value)}
+        className="w-full p-4 border border-gray-600 rounded-xl"
+      />
+    </div>
+
+    <div className="flex flex-col space-y-2">
+      <label>최대 수용 인원</label>
+      <input
+        type="number"
+        required
+        max={10}
+        value={dataGuests}
+        onChange={(e) => setDataGuests(e.target.value)}
+        className="w-full p-4 border border-gray-600 rounded-xl"
+      />
+    </div>
+  </div>
+
+        <CustomButton
+          label="다음"
+          type="submit"
+        />            
+</form>        
+
+
         </> 
-      )
+      ) : currentStep === 4 ?
+       (
+        <>
+
+        </>
+      ) : null}
+
       
-      }
-    </>
+     </>
+      
+    
   );
 
   return (
