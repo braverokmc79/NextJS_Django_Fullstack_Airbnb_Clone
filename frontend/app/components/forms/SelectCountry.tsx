11da2b02fef6c61646ccbc5f1e058edec7442703
braverokmc79 <braverokmc79@gmail.com>
2025-07-15 21:42:20 +0900

    5-5.location.setting

---
+++ frontend/app/components/forms/SelectCountry.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import useCountries from '@/app/hooks/useCountries';
+import Select from 'react-select';
+
+
+export type SelectCountryValue = {
+    label: string;
+    value: string;
+}
+
+interface SelectCountryProps {
+    value?: SelectCountryValue;
+    onChange: (value: SelectCountryValue) => void;
+}
+
+const SelectCountry: React.FC<SelectCountryProps> = ({
+    value,
+    onChange
+}) => {
+    const {getAll} = useCountries();
+
+    return (
+        <>
+            <Select
+                isClearable
+                placeholder="Anywhere"
+                options={getAll()}
+                value={value}
+                onChange={(value) => onChange(value as SelectCountryValue)}
+            />
+        </>
+    )
+}
+
+export default SelectCountry;
+
+
+//설치
+//pnpm install react-select
+
+//pnpm install world-countries
